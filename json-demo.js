755a8da124ce7a403f33923534518546b66fa57f
Diego Aarón <diegoaarondv@gmail.com>
2022-10-26 23:21:55 -0500

    manipulando json

--- json-demo.js
+++ json-demo.js
@@ -3,3 +3,16 @@ const curso = require('./curso.json');
 console.log(typeof curso);
 
 console.log(curso.titulo);
+
+let infoCurso = {
+    "titulo": "aprende node.js",
+    "numVistas": 45625,
+    "numLikes": 21123,
+    "temas": [
+      "javascript",
+      "node.js"
+    ],
+    "esPublico": true
+  }
+
+  console.log(infoCurso);
