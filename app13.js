aa064e0f918d895d52c8b6d6095e8976987db6b8
Diego Aarón <diegoaarondv@gmail.com>
2022-11-07 20:21:39 -0500

    manejando URLS

--- app13.js
+++ app13.js
@@ -6,4 +6,9 @@ console.log(miURL.pathname); // /cursos/programacion
 
 console.log(miURL.searchParams); // URLSearchParams { 'ordenar' => 'vistas', 'nivel' => '1' }
 
+console.log(typeof miURL.searchParams); // object
+
+console.log(miURL.searchParams.get('ordenar'));
+
+console.log(miURL.searchParams.get('nivel'));
 
