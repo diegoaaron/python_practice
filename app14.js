d620dac7036f7c00fff95337adc5562ed78e3cfd
Diego Aarón <diegoaarondv@gmail.com>
2022-11-07 22:48:18 -0500

    enrutamiento en node.js

--- app14.js
+++ app14.js
@@ -9,10 +9,39 @@ const servidor = http.createServer((req, res) => {
     switch(method) {
         case 'GET':
             return manejarSolicitudGET(req, res);
+        case 'POST':
+            return manejarSolicitudPOST(req, res);
         default:
             console.log(`El metodo utilizado no puede ser manejado por el servido: ${method}`);
     }
 });
+
+function manejarSolicitudGET(req, res) {
+    const path = req.url;
+
+    if(path === '/') {
+        res.statusCode = 200;
+        return res.end('Bienvenido a mi primer servidor y API creado con Nodejs');
+    } else if (path === '/cursos') {
+        res.statusCode = 200;
+        return res.end(JSON.stringify(cursos.infoCursos));
+    } else if (path == '/cursos/programacion') {
+        res.statusCode = 200;
+        return res.end(JSON.stringify(cursos.infoCursos.programacion));
+    }
+
+    res.statusCode = 404;
+    return res.end('El recurso solicitado, no existe...');
+}
+
+function manejarSolicitudPOST(req, res) {
+    const path = req.url;
+
+    if (path === '/cursos/programacion') {
+        res.statusCode = 200;
+        return res.end('El servidor recibio una solicitud POST para /cursos/programacion');
+    }
+}
 
 const PUERTO = 3000;
 
