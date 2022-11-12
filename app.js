dc498370ed9fb6809cde31b97a38c550d884c877
Diego Aarón <diegoaarondv@gmail.com>
2022-11-12 11:08:40 -0500

    express basico

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const app = express();
 const {infoCursos} = require('./cursos.js'); // obteniendo los archivos con sintaxis de desestructuracion
 
 
+// Routing
+
+app.get('/', (req, res) => {
+    res.send('Mi primer servidor con express. Cursos');
+});
+
+
+// puerto 
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+    console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`);
+});
+
