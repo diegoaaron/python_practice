9c58675e164d7a781c0574db2f7980c40daf5013
Diego Aarón <diegoaarondv@gmail.com>
2022-11-12 11:21:03 -0500

    express basico

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
     res.send('Mi primer servidor con express. Cursos');
 });
 
+app.get('/api/cursos', (req, res) => {
+    //res.send(infoCursos); // formato normal
+    res.send(JSON.stringify(infoCursos)); // formato JSON
+});
+
+app.get('/api/cursos/programacion', (req, res) => {
+    res.send(JSON.stringify(infoCursos.programacion));
+});
+
+app.get('/api/cursos/matematicas', (req,res) => {
+    res.send(JSON.stringify(infoCursos.matematicas));
+});
 
 // puerto 
 const PUERTO = process.env.PORT || 3000;
