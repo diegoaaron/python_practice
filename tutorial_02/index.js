ed1c3cf40791bd2d4e652aa771df8ac7d42e9701
Diego Aarón <diegoaarondv@gmail.com>
2022-11-14 11:50:51 -0500

    probando javascript con node

---
+++ tutorial_02/index.js
@@ -0,0 +1,36 @@
+let username = 'fast';
+
+let age = 14;
+
+let hasHobbies = false;
+
+let points = [10,20,30];
+
+let user = {
+    name: 'diego',
+    lastname: 'ray'
+}
+
+const PI = 3.1415;
+
+console.log(username);
+console.log(age);
+console.log(hasHobbies);
+console.log(points);
+console.log(user);
+
+
+if(age >= 18) {
+    console.log("eres un adulto");
+} else if(age >= 13) {
+    console.log("eres un adolecente");
+} else {
+    console.log("eres un niño");
+}
+
+const otrosNombres = ['joe','jhon','luis'];
+
+for(let i = 0; i< otrosNombres.length; i++) {
+    console.log(otrosNombres[i]);
+}
+
