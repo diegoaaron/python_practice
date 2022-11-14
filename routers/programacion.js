23579033d2067037ccb3855d7fc8334c1077eeaa
Diego Aarón <diegoaarondv@gmail.com>
2022-11-13 20:16:42 -0500

    probando los metodos POST PUT PATCH y DELETE

--- routers/programacion.js
+++ routers/programacion.js
@@ -51,5 +51,55 @@ routerProgramacion.post('/',(req, res) => {
     res.send(JSON.stringify(programacion));
 });
 
+
+// PUT envia todas las propiedades de un elemento
+
+routerProgramacion.put('/:id', (req, res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if (indice >= 0) {
+        programacion[indice] = cursoActualizado;
+    } else {
+        return res.status(404).send(`No se encontro el item`);
+    }
+
+    res.send(JSON.stringify(programacion));
+
+});
+
+// PATCH, permite enviar solo las propiedades especificas que se modifican del objeto 
+
+routerProgramacion.patch('/:id',(req, res) => {
+    const infoActualizada = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if (indice >= 0) {
+        const cursoAModificar = programacion[indice];
+        Object.assign(cursoAModificar,infoActualizada);
+    }
+
+    res.send(JSON.stringify(programacion));
+
+});
+
+// DELETE elimina 
+
+routerProgramacion.delete('/:id',(req,res) => {
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if (indice >= 0) {
+        programacion.splice(indice, 1);
+    }
+
+    res.send(JSON.stringify(programacion));
+});
+
 module.exports = routerProgramacion;
 
