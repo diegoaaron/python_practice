13782f57e5ed2291039195a9452ea17e24933d58
Diego Aarón <diegoaarondv@gmail.com>
2022-11-15 13:20:20 -0500

    fs async

---
+++ tutorial_02/fs.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+
+const first = fs.readFileSync('./data/first.txt','utf-8');
+
+const second = fs.readFileSync('./data/second.txt');
+
+
+console.log(first);
+console.log(second.toString());
+
+const title = 'este es el contenido del archivo';
+
+fs.writeFileSync('./data/third.txt',title);
+
