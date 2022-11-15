601aa99a83d94b9f07868f338bb8239a95cc9e47
Diego Aarón <diegoaarondv@gmail.com>
2022-11-15 13:49:49 -0500

    fs async

--- tutorial_02/fs.js
+++ tutorial_02/fs.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-
+/*
 const first = fs.readFileSync('./data/first.txt','utf-8');
 
 const second = fs.readFileSync('./data/second.txt');
@@ -12,4 +12,26 @@ console.log(second.toString());
 const title = 'este es el contenido del archivo';
 
 fs.writeFileSync('./data/third.txt',title);
+*/
 
+fs.readFile('./data/first.txt','utf-8',(error, data) => {
+
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+
+        fs.readFile('./data/second.txt','utf-8',(error, data) => {
+
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data);
+                fs.writeFile('./data/newfile.txt','archivo creado desde fs', (error, data) => {
+                    console.log(error);
+                    console.log(data);
+                });
+            }
+        });
+    }
+});
