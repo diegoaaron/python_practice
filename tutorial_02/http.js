b7e7b6ed3b0f9df857053a91d5273110548cacb2
Diego Aarón <diegoaarondv@gmail.com>
2022-11-18 10:51:56 -0500

    http request

--- tutorial_02/http.js
+++ tutorial_02/http.js
@@ -1,8 +1,25 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-    response.write('hello world');
+
+    console.log(request.url);
+
+    if(request.url === '/') {
+        response.write('hello world');
+        return response.end();
+    }
+    
+    if (request.url === '/about') {
+        response.write('estas en about');
+        return response.end();
+    }
+
+    response.write(`<h1>Not found</h1>
+    <p>esta página no se encontro</p>
+    <a href="/">voler a la pagina principal</a>`);
     response.end();
+
+    
 }).listen(3000);
 
 console.log('servidor en el puerto 3000');
