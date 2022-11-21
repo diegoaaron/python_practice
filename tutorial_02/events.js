18b5145a7a7e04a5e3c28d760be4643a9cd62545
Diego Aarón <diegoaarondv@gmail.com>
2022-11-21 12:45:39 -0500

    eventos nodejs

--- tutorial_02/events.js
+++ tutorial_02/events.js
@@ -2,11 +2,12 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter();
 
-customEmitter.on('response', (data) => {
+customEmitter.on('response', (data,secondData) => {
     console.log('recibido');
     console.log(data);
+    console.log(secondData);
 });
 
 // emitiendo 
 
-customEmitter.emit('response','hello word');
+customEmitter.emit('response','hello word',[1,2,35]);
