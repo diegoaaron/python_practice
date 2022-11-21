de596265180fd2da7f4ac3c7ea139eab6abc2236
Diego Aarón <diegoaarondv@gmail.com>
2022-11-21 03:56:55 -0500

    event loop

--- tutorial_02/event_loop.js
+++ tutorial_02/event_loop.js
@@ -1,8 +1,24 @@
-console.log("first");
+const { readdirSync } = require('fs');
+const http = require('http');
 
-setTimeout(() => {
-    console.log("second");
-},0);
+const server = http.createServer((req,res) => {
+    if(req.url === '/') {
+        res.write("hola");
+        return res.end();
+    }
 
+    if(req.url === '/about') {
 
-console.log("third");
+        for(let i = 0; i <100000; i++){
+            console.log(Math.random()*i);
+        }
+
+        return res.end('About page');
+    }
+
+    res.end('not found');
+
+});
+
+server.listen(3000);
+console.log('server on 3000');
