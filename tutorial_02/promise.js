9415f871c0d13c7750465f5faf534ff8930fe592
Diego Aarón <diegoaarondv@gmail.com>
2022-11-21 10:14:19 -0500

    promesas

---
+++ tutorial_02/promise.js
@@ -0,0 +1,18 @@
+const {readFile} = require('fs');
+
+const getText = (pathFile) => {
+    return new Promise(function(resolve, reject) {
+        readFile(pathFile, 'utf-8', (err, data) => {
+            if(err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
+getText('./data/first.txt')
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+
+ 
