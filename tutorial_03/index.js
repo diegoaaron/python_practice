a2044bd4f8790bca754efecd06f4f4f185a24c6f
Diego Aarón <diegoaarondv@gmail.com>
2022-11-24 08:58:50 -0500

    routing

--- tutorial_03/index.js
+++ tutorial_03/index.js
@@ -3,12 +3,17 @@ const express =require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.sendFile('./static/index.html',{
-        root: __dirname
-    });
+    res.send('hello world');
 });
 
+app.get('/about',(req, res) => {
+    res.send('About');
+});
+
+app.get('/weather',(req, res) => {
+    res.send('the current weather is nice');
+});
 
 app.listen(3000);
-console.log('server en puerto 3000');
+console.log(`server en puerto ${3000}`);
 
