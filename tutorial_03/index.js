f55a0c9e7d2ddc680e01c4d303d2b19aa71fde8f
Diego Aarón <diegoaarondv@gmail.com>
2022-11-24 09:08:54 -0500

    routing

--- tutorial_03/index.js
+++ tutorial_03/index.js
@@ -14,6 +14,13 @@ app.get('/weather',(req, res) => {
     res.send('the current weather is nice');
 });
 
+// manejador de las rutas que no existen en el servidor
+app.use((req, res) => {
+    res.status(404);
+    res.send('no se encontro la ruta');
+});
+
+
 app.listen(3000);
 console.log(`server en puerto ${3000}`);
 
