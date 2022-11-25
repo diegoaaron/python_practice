62f44794c39b9dcc9169e56b5e2ff2497fadb9aa
Diego Aarón <diegoaarondv@gmail.com>
2022-11-25 10:22:58 -0500

    request body

--- tutorial_03/index4.js
+++ tutorial_03/index4.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app= express();
 
 
-
+app.post('/user', (req,res) => {
+    res.send('nuevo usuario creado');
+})
 
 app.listen(3000);
 console.log(`server en puerto ${3000}`);
