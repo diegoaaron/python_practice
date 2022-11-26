13e2905ef6ee24161b9df7886044398d52180731
Diego Aarón <diegoaarondv@gmail.com>
2022-11-26 14:12:18 -0500

    request parameter

--- tutorial_03/index5.js
+++ tutorial_03/index5.js
@@ -13,7 +13,24 @@ app.get('/add/:x/:y', (req, res) => {
     // utilizando la desectructracion de JS
     const {x, y} = req.params;
     res.send(`resultado: ${parseInt(x) + parseInt(y)}`);
-})
+});
+
+app.get('/users/:username/photo', (req, res) => {
+    console.log(req.params.username);
+    if(req.params.username === 'fazt') {
+        return res.sendFile('./javascript.png',{
+            root: __dirname
+        });
+    } else {
+        res.send('el usuario no tiene acceso');
+    }
+});
+
+app.get('/name/:nombre/age/:age', (req,res) => {
+    res.send(`El usuario ${req.params.nombre} tiene ${req.params.age} años`);
+});
+
+
 
 app.listen(3000);
 console.log('servidor en puerto 3000');
