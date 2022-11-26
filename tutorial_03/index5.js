a408ac8e7b0a11c74294918e9bbe37beb7f625cd
Diego Aarón <diegoaarondv@gmail.com>
2022-11-26 14:01:25 -0500

    request parameter

--- tutorial_03/index5.js
+++ tutorial_03/index5.js
@@ -5,8 +5,15 @@ const app = express();
 app.get('/hello/:user', (req, res) => {
     console.log(req.params);
     console.log(typeof req.params.user);
-    res.send(`hello ${req.params.user}`);
+    res.send(`hello ${req.params.user.toUpperCase()}`);
 });
+
+app.get('/add/:x/:y', (req, res) => {
+
+    // utilizando la desectructracion de JS
+    const {x, y} = req.params;
+    res.send(`resultado: ${parseInt(x) + parseInt(y)}`);
+})
 
 app.listen(3000);
 console.log('servidor en puerto 3000');
