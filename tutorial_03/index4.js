d8016fdcb62159232ec092152b07f4126817c647
Diego Aarón <diegoaarondv@gmail.com>
2022-11-26 13:36:43 -0500

    request body

--- tutorial_03/index4.js
+++ tutorial_03/index4.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app= express();
 
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.post('/user', (req,res) => {
+    console.log(req.body);
     res.send('nuevo usuario creado');
 })
 
