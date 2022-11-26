6bf4b9ff3164422e018e3a49d0878f8b3d907b85
Diego Aarón <diegoaarondv@gmail.com>
2022-11-26 14:41:04 -0500

    request queries

---
+++ tutorial_03/index6.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/search', (req, res) => {
+    if(req.query.q === 'javascript books') {
+        res.send('lista de libros javascprit');
+    } else {
+        res.send('pagina normal');
+    }
+});
+
+app.listen(3000);
+console.log('server en puerto 3000');
