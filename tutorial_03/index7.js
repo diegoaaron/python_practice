ef15d9b228906becd8be3cbc1d453da7f859beaf
Diego Aarón <diegoaarondv@gmail.com>
2022-11-26 23:58:36 -0500

    middleware

--- tutorial_03/index7.js
+++ tutorial_03/index7.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.use((req,res, next) => {
+    console.log('paso por aqui');
+    console.log(`route: ${req.url} motodo: ${req.method}`);
+    next();
+});
+
 app.get('/profile', (req, res) => {
     res.send('profile page');
 });
+
+app.all('/about', (req,res) => {
+    res.send('about page');
+})
 
 app.listen(3000);
 console.log('server en puerto 3000');
