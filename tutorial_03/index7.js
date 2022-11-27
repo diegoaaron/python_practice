4aa762347cde6b91511c769090d04062d15b1590
Diego Aarón <diegoaarondv@gmail.com>
2022-11-26 23:47:51 -0500

    midleware

--- tutorial_03/index7.js
+++ tutorial_03/index7.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
-
+app.get('/profile', (req, res) => {
+    res.send('profile page');
+});
 
 app.listen(3000);
 console.log('server en puerto 3000');
