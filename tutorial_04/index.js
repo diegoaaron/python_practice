4a233c73019f3bbc7b5b5cef4cceef009e6512cf
Diego Aarón <diegoaarondv@gmail.com>
2022-12-04 22:48:21 -0500

    creando servidor

--- tutorial_04/index.js
+++ tutorial_04/index.js
@@ -2,5 +2,14 @@ import express from 'express';
 
 const app = express();
 
+
+app.get('/employees', (req,res) => res.send('obteniendo empleados'));
+
+app.post('/employees', (req,res) => res.send('creando empleados'));
+
+app.put('/employees', (req,res) => res.send('actualizando empleados'));
+
+app.delete('/employees', (req,res) => res.send('eliminando empleado'));
+
 app.listen(3000);
-console.log('servidor en puerto 3000');
+console.log('servidor ejecutandose en puerto 3000');
