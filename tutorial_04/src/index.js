0d2903f4ee35e88e794645c33a8172f7ddc129fc
Diego Aarón <diegoaarondv@gmail.com>
2022-12-05 17:46:12 -0500

    router

--- tutorial_04/src/index.js
+++ tutorial_04/src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import {pool} from './db.js';
+
+import employeesRoutes from './routes/employees.routes.js';
+
 const app = express();
 
 app.get('/ping', async (req,res) => {
@@ -7,13 +10,7 @@ app.get('/ping', async (req,res) => {
     res.json(result[0]);
 });
 
-app.get('/employees', (req,res) => res.send('obteniendo empleados'));
-
-app.post('/employees', (req,res) => res.send('creando empleados'));
-
-app.put('/employees', (req,res) => res.send('actualizando empleados'));
-
-app.delete('/employees', (req,res) => res.send('eliminando empleado'));
+app.use(employeesRoutes);
 
 app.listen(3000);
 console.log('servidor ejecutandose en puerto 3000');
