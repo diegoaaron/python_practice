3489ad4f283e97f2b78bd64f2d5cab3830b25ac8
Diego Aarón <diegoaarondv@gmail.com>
2022-12-05 18:02:55 -0500

    router

--- tutorial_04/src/index.js
+++ tutorial_04/src/index.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import {pool} from './db.js';
+
 
 import employeesRoutes from './routes/employees.routes.js';
+import indexRoutes from './routes/index.router.js';
 
 const app = express();
 
-app.get('/ping', async (req,res) => {
-    const [result] = await pool.query('SELECT 1 + 1 as result');
-    res.json(result[0]);
-});
-
+app.use(indexRoutes);
 app.use(employeesRoutes);
 
 app.listen(3000);
