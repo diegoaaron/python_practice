ed241c123323f3bbfd4a0e1b9a4980b77b5c9160
Diego Aarón <diegoaarondv@gmail.com>
2022-12-04 23:35:29 -0500

    conexion a bd

--- tutorial_04/index.js
+++ tutorial_04/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
-
+import {pool} from './db.js';
 const app = express();
 
+app.get('/ping', async (req,res) => {
+    const [result] = await pool.query('SELECT 1 + 1 as result');
+    res.json(result[0]);
+});
 
 app.get('/employees', (req,res) => res.send('obteniendo empleados'));
 
