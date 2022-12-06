fd227b314f424ca9c46da3ec498ae007d863d99a
Diego Aarón <diegoaarondv@gmail.com>
2022-12-05 20:24:37 -0500

    get items

--- tutorial_04/src/index.js
+++ tutorial_04/src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(indexRoutes);
-app.use(employeesRoutes);
+app.use('/api',employeesRoutes);
 
 app.listen(3000);
 console.log('servidor ejecutandose en puerto 3000');
