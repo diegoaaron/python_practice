9dd9ce3ae76509dfb62846f488384d7d437aa425
Diego Aarón <diegoaarondv@gmail.com>
2022-12-05 21:17:24 -0500

    delete rows

--- tutorial_04/src/controllers/employees.controller.js
+++ tutorial_04/src/controllers/employees.controller.js
@@ -24,7 +24,15 @@ export const createEmployees = async (req,res) => {
     salary });
 }
 
+export const deleteEmployees =  async (req,res) => {
+    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id]);
+    if (result.affectedRows <= 0) return res.status(404).json({
+        message: 'Employee not found'
+    });
+    res.sendStatus(204);
+};
+
 export const updateEmployees = (req,res) => res.send('actualizando empleados');
 
-export const deleteEmployees =  (req,res) => res.send('eliminando empleado');
+
 
