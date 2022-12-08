217aa27da285604906b06a7a37f67b3cb8c67dc9
Diego Aarón <diegoaarondv@gmail.com>
2022-12-08 12:18:41 -0500

    config envieroment

--- tutorial_04/src/index.js
+++ tutorial_04/src/index.js
@@ -1,23 +1,5 @@
-import express from 'express';
-
-import employeesRoutes from './routes/employees.routes.js';
-import indexRoutes from './routes/index.router.js';
-
+import app from './app.js';
 import {PORT} from './config.js';
-
-const app = express();
-
-app.use(express.json());
-
-app.use(indexRoutes);
-app.use('/api',employeesRoutes);
-
-// midleware
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'endpoint not found'
-    });
-});
 
 app.listen(PORT);
 console.log(`servidor ejecutandose en puerto ${PORT}`);
