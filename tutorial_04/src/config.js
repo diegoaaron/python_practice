94a146e8bc3106db8f7128379ae24673ac745672
Diego Aarón <diegoaarondv@gmail.com>
2022-12-08 12:06:32 -0500

    config envieroment

--- tutorial_04/src/config.js
+++ tutorial_04/src/config.js
@@ -2,13 +2,8 @@ import { config } from "dotenv";
 
 config();
 
-export const PORT = process.env.PORT || 3000
-
-
-/*
-DB_HOST=localhost
-DB_PORT=3306
-DB_USER=root
-DB_PASSWORD=diego989
-DB_DATABASE=companydb
-*/
+export const PORT = process.env.PORT || 3000;
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'diego989';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.PORT || 3306;
