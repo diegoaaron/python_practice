7b36b2dd4908689c271dcdf55625d293f3eaa608
Diego Aarón <diegoaarondv@gmail.com>
2022-12-11 17:17:16 -0500

    create template

---
+++ tutorial_05/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+
+// configuración de pug
+app.set('views', './vistas');
+app.set('view engine', 'pug');
+
+// configuracion de archivos estaticos
+app.use(express.static('./vistas'))
+app.use(express.static('./src'))
+app.use(express.static('./css'))
+
+// get raiz
+app.get('/', (req, res) => {
+    //res.send('aplicacion inidicada... todo va bien');
+    res.render('index', {titulo:'aplicacion de contactos', dato:'cualquier texto'});
+});
+
+
+
+// configuracion del peruto
+app.listen('8000', function() {
+    console.log('aplicacion iniciada en el puerto 8000');
+});
