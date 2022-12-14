0a51e7c13a1b18052cfaf3876f1fc3431546d16a
Diego Aarón <diegoaarondv@gmail.com>
2022-12-14 13:13:28 -0500

    template login

--- tutorial_06/client/src/App.js
+++ tutorial_06/client/src/App.js
@@ -1,24 +1,63 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route
+  Route,
+  Outlet
 } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is Home!</div>,
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      }
+    ]
   },
   {
-    path: "/test",
-    element: <div>This is Test!</div>,
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
   },
 ]);
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router}/>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>
     </div>
   );
 }
