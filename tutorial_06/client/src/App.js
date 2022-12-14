0e0987254be0f4248a6e62604efcb3ae2dcbfd51
Diego Aarón <diegoaarondv@gmail.com>
2022-12-14 12:03:54 -0500

    add routing client

--- tutorial_06/client/src/App.js
+++ tutorial_06/client/src/App.js
@@ -1,7 +1,24 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is Home!</div>,
+  },
+  {
+    path: "/test",
+    element: <div>This is Test!</div>,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router}/>
     </div>
   );
 }
