e38f0ae4953cd010f8941974ac1763330bf15f35
Diego Aarón <diegoaarondv@gmail.com>
2023-04-14 01:24:07 -0500

    book 1 - 1

---
+++ pyt_crash_course/cap2.py
@@ -0,0 +1,35 @@
+# definiendo variables
+
+print("Hello Python world!")
+
+message = "Hello Python CRASH world!"
+print(message)
+
+name = "ADA lovelace"
+print(name.title())
+
+print(name.upper())
+
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+
+full_name = first_name + " " + last_name
+
+print(full_name.title())
+
+print("Hello " + full_name.upper() + "!")
+
+
+print("\t tabulacion")
+print("\n saldo de linea \"")
+print()
+
+full_st = " pythonn   "
+
+sd = full_st.rstrip()
+
+print("1" + sd + "2")
+
+# PAG 30
