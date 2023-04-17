f62f8549de697c99720eeb5b174b2931da8d6695
Diego Aarón <diegoaarondv@gmail.com>
2023-04-17 10:15:07 -0500

    book 1 - 15

--- pyt_crash_course/cap7.py
+++ pyt_crash_course/cap7.py
@@ -8,3 +8,38 @@ promt += "\nCual es tu primero nombre: "
 
 name = input(promt)
 print("\nHola " + name + "!")
+
+numero = input("ingrese un numero para ver si es impar: ")
+numero = int(numero)
+
+if numero % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
+
+# utilizando WHILE para recibir datos indefinidos
+
+message = ""
+
+prompt = "Indicanos algo a repetir: "
+prompt += "\nIngresesa 'quit' para salir del programa: "
+
+while message != 'quit':
+    message = input(prompt)
+    print(message)
+
+# utilizando BREAK y WHILE para recibir datos indefinidos
+
+message = ""
+
+prompt = "Indicanos algo a repetir: "
+prompt += "\nIngresesa 'quit' para salir del programa: "
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("lo ingresado es: " + city)
+
