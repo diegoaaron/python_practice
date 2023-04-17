d4a5ca0ff52f7adf52673eff85e75338f9aa49c2
Diego Aarón <diegoaarondv@gmail.com>
2023-04-17 09:45:47 -0500

    book 1 - 14

--- pyt_crash_course/cap7.py
+++ pyt_crash_course/cap7.py
@@ -1,1 +1,10 @@
 # pag 117
+
+mensaje = input("Ingrese algun texto: ")
+print(mensaje)
+
+promt = "Dinos quien eres para personalizar tu mensaje."
+promt += "\nCual es tu primero nombre: "
+
+name = input(promt)
+print("\nHola " + name + "!")
