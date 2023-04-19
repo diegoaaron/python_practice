40913bbec43439d7c9654361a7682668bbb60f22
Diego Aarón <diegoaarondv@gmail.com>
2023-04-19 10:56:17 -0500

    book 1 - 18

--- pyt_crash_course/cap8.py
+++ pyt_crash_course/cap8.py
@@ -1,1 +1,20 @@
-# PAG 134
+# parametro: denominacion que la funcion necesita para trabajar internamente
+# argumento: elemento que es pasado a una funcion cuando se la llama.
+
+# funcion con parametros
+
+def greet_user(username):
+    print("Hola, " + username.title() + "!")
+
+greet_user("jordan")
+
+# Tipos de argumentos
+# Argumentos posicionales: Es cuando los argumentos pasados a una funcion son asociados con los 
+# parametros en base al orden de los argumentos. Se debe respetar el orden de los parametros
+
+def descripcion_mascota(raza, nombre):
+    print("Mi mascota " + nombre)
+    print("es de raza " + raza)
+
+descripcion_mascota("pastor aleman", "toffy")
+
