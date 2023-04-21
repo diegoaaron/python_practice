5e1ac796fc67e45ca4fe32d291a59032c2fbefab
Diego Aarón <diegoaarondv@gmail.com>
2023-04-20 23:52:52 -0500

    book 1 - 24

--- pyt_crash_course/cap9.py
+++ pyt_crash_course/cap9.py
@@ -1,4 +1,8 @@
 # Definiendo una clase
+# El metodo __init___() es el constructor de la clase y por ende es el primer
+# metodo ejecutado al crear una instancia de la clase 
+# el parametro self, se pasa automaticamente por lo cual no debemos indicarlo
+# al crear una instancia de la clase
 
 class Dog():
     def __init__(self, name, age):
@@ -10,6 +14,8 @@ class Dog():
 
     def roll_over(self):
         print(self.name.title() + " rolled over!")
+
+#
 
 animal = Dog("toffy", 4)
 
