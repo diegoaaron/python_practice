dcc60e2702cdb61e130a44ca16bbd58e797d6448
Diego Aarón <diegoaarondv@gmail.com>
2023-04-22 18:30:41 -0500

    book 1 - 35

--- pyt_crash_course/name_function.py
+++ pyt_crash_course/name_function.py
@@ -5,3 +5,23 @@ def get_formatted_name(first, last, middle = ""):
         full_name = first + ' ' + last
     return full_name.title()
 
+# Clase a testear
+
+class AnonymousSurvey():
+
+    def __init__(self, question):
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        print(self.question)
+
+    def store_response(self, new_response):
+        self.responses.append(new_response)
+
+    def show_results(self):
+        print("survey results")
+        for response in self.responses:
+            print("- " + response)
+
+
