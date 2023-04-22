12a91ead2d225afb4c7bc85c5d9637f49a24b072
Diego Aarón <diegoaarondv@gmail.com>
2023-04-22 18:08:39 -0500

    book 1 - 34

--- pyt_crash_course/cap11.py
+++ pyt_crash_course/cap11.py
@@ -1,10 +1,18 @@
 import unittest
 from name_function import get_formatted_name
 
+# utlizamos el modulo "unittest" para ordenar nuestros test unitarios
+# utilizamos una funcion que esta en otro modulo 
+
 class NamesTestCase(unittest.TestCase):
 
     def test_first_last_name(self):
         formatted_name = get_formatted_name("janis", "joplin")
         self.assertEqual(formatted_name, "Janis Joplin")
 
+    def test_first_last_middle_name(self):
+        formatted_name = get_formatted_name("wolfgang", "mozart", "amadeus")
+        self.assertEqual(formatted_name, "Wolfgang Amadeus Mozart")
+
 unittest.main()
+
