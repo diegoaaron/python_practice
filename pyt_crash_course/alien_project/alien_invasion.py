462345f43d01d8e5afd147659e07106c7a1636ed
Diego Aarón <diegoaarondv@gmail.com>
2023-04-24 13:19:57 -0500

    book 1 - 39

---
+++ pyt_crash_course/alien_project/alien_invasion.py
@@ -0,0 +1,21 @@
+import sys
+
+import pygame 
+
+def run_game():
+    # iniciamos la pantalla 
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("Alien invasion")
+
+    # Iniciamos el loop principal del juego
+    while True:
+
+        #Esperando el evento de cierre
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        pygame.display.flip()
+
+run_game()
