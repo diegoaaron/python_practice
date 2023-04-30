a9cb8d403d1f2449734b31be057715f42305e30e
Diego Aarón <diegoaarondv@gmail.com>
2023-04-30 00:05:03 -0500

    book 1 - 53

--- pyt_crash_course/alien_project/settings.py
+++ pyt_crash_course/alien_project/settings.py
@@ -10,3 +10,9 @@ class Settings():
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 0.2
+
+        # configuracion de las balas
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
