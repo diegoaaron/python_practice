8b0286fbfefc7ccbcab987852a6bc4717d7e1592
Diego Aarón <diegoaarondv@gmail.com>
2023-05-03 09:21:01 -0500

    book 1 - 83

--- pyt_crash_course/generation_data/mpl_squares.py
+++ pyt_crash_course/generation_data/mpl_squares.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
 
-squares = [1,4,9,16,25]
-plt.plot(squares)
+imput_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(imput_values ,squares, linewidth = 5)
+
+# configurando parametro sde la gráfica
+
+plt.title("Cuadrados", fontsize = 24)
+plt.xlabel("Valores", fontsize = 14)
+plt.ylabel("Cuadrados", fontsize = 14)
+
+plt.tick_params(axis = 'both', labelsize = 14)
+
 plt.show()
