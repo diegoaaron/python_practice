928a9259cab5c0adf286f7e4e14d77ae8574d6d5
Diego Aarón <diegoaarondv@gmail.com>
2023-05-06 23:48:30 -0500

    book 1 - 94

--- pyt_crash_course/format_data/highs_lows.py
+++ pyt_crash_course/format_data/highs_lows.py
@@ -1,5 +1,7 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 filename = "sitka_weather_07-2014.csv"
 
 with open(filename) as f:
@@ -13,3 +15,17 @@ with open(filename) as f:
         highs.append(high)
 
     print(highs)
+
+# plot data
+
+fig = plt.figure(dpi=128, figsize=(10,6))
+plt.plot(highs, c="red")
+
+# format plot
+
+plt.title("Temperaturas altas de julio 2014", fontsize = 24)
+plt.xlabel('', fontsize = 16)
+plt.ylabel("Temperatura (F)", fontsize = 16)
+plt.tick_params(axis="both", which="major", labelsize=16)
+
+plt.show()
