329a1e30982610631ddc8a661bbea30be0335826
Diego Aarón <diegoaarondv@gmail.com>
2023-05-06 23:56:17 -0500

    book 1 - 95

--- pyt_crash_course/format_data/highs_lows.py
+++ pyt_crash_course/format_data/highs_lows.py
@@ -1,5 +1,7 @@
 import csv
 
+from datetime import datetime
+
 from matplotlib import pyplot as plt
 
 filename = "sitka_weather_07-2014.csv"
@@ -9,8 +11,12 @@ with open(filename) as f:
     header_row = next(reader)
 
     highs = []
+    dates = []
 
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
@@ -19,12 +25,13 @@ with open(filename) as f:
 # plot data
 
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(highs, c="red")
+plt.plot(dates, highs, c="red")
 
 # format plot
 
 plt.title("Temperaturas altas de julio 2014", fontsize = 24)
 plt.xlabel('', fontsize = 16)
+fig.autofmt_xdate()
 plt.ylabel("Temperatura (F)", fontsize = 16)
 plt.tick_params(axis="both", which="major", labelsize=16)
 
