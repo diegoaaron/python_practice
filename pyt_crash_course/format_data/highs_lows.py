622ea16101d759b0f7b954630c5762036ef3f704
Diego Aarón <diegoaarondv@gmail.com>
2023-05-06 23:40:22 -0500

    book 1 - 93

--- pyt_crash_course/format_data/highs_lows.py
+++ pyt_crash_course/format_data/highs_lows.py
@@ -6,5 +6,9 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+
+    for row in reader:
+        highs.append(row[1])
+
+    print(highs)
