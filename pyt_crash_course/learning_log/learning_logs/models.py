b8a7f8c3d23f683a850f7e97184f8e166a0343b7
Diego Aarón <diegoaarondv@gmail.com>
2023-05-07 18:38:42 -0500

    book 1 - 110

--- pyt_crash_course/learning_log/learning_logs/models.py
+++ pyt_crash_course/learning_log/learning_logs/models.py
@@ -6,6 +6,23 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+
+
     def __str__(self):
         """Retorna una cadena que representa al objeto del modelo"""
         return self.text
+    
+
+class Entry(models.Model):
+    """Algo especiico para aprender"""
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Representa un string para el modelo"""
+        return self.text[:50] + "..."
+    
