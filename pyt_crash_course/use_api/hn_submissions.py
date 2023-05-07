005c3c287c61b588b42fac47d5f5069d4f1fd06a
Diego Aarón <diegoaarondv@gmail.com>
2023-05-07 16:01:50 -0500

    book 1 - 108

--- pyt_crash_course/use_api/hn_submissions.py
+++ pyt_crash_course/use_api/hn_submissions.py
@@ -2,7 +2,34 @@ import requests
 
 from operator import itemgetter
 
-url = "https://hacker-news.firebaseio.com/v0/item/9884165.json"
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 
 r = requests.get(url)
-print("status code: " + r.status_code)
+print("status code: " + str(r.status_code))
+
+# Procesando información
+
+submission_ids = r.json()
+submission_dicts = []
+
+for submission_id in submission_ids[:30]:
+    # Haciendo un url diferente para cada submission
+    url = ('https://hacker-news.firebaseio.com/v0/item/' + str(submission_id) + '.json')
+    submission_r = requests.get(url)
+    print(submission_r.status_code)
+    response_dict = submission_r.json()
+
+    submission_dict = {
+        "title": response_dict['title'],
+        "link": 'http://news.ycombinator.com/item?id=' + str(submission_id),
+        "comments": response_dict.get('descendants', 0)
+    }
+
+    submission_dicts.append(submission_dict)
+
+submission_dicts = sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+
+for submission_dict in submission_dicts:
+    print("\nTitle: " + submission_dict["title"])
+    print("Enlace: " + submission_dict["link"])
+    print("Comentarios " + str(submission_dict["comments"]))
