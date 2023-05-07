e677ef09421b163ed9f42c885b54f1707868baa7
Diego Aarón <diegoaarondv@gmail.com>
2023-05-07 12:26:54 -0500

    book 1 - 101

--- pyt_crash_course/format_data/highs_lows.py
+++ pyt_crash_course/format_data/highs_lows.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # filename = "sitka_weather_07-2014.csv"
-filename = "sitka_weather_2014.csv"
-
+# filename = "sitka_weather_2014.csv"
+filename = "death_valley_2014.csv"
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -17,16 +17,17 @@ with open(filename) as f:
     lows = []
 
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
-
-    print(highs)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+            
+        except ValueError:
+            print(current_date, "FALTA DATA")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # plot data
 
@@ -37,7 +38,7 @@ plt.fill_between(dates, highs, lows, facecolor="blue", alpha = 0.1)
 
 # format plot
 
-plt.title("Temperaturas de 2014", fontsize = 24)
+plt.title("Temperaturas de 2014", fontsize = 20)
 plt.xlabel('', fontsize = 16)
 fig.autofmt_xdate()
 plt.ylabel("Temperatura (F)", fontsize = 16)
