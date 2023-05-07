c757a92f4f3dfb84a6ead4487ccb04f144ef8f35
Diego Aarón <diegoaarondv@gmail.com>
2023-05-07 12:22:01 -0500

    book 1 - 100

--- pyt_crash_course/format_data/highs_lows.py
+++ pyt_crash_course/format_data/highs_lows.py
@@ -4,7 +4,9 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-filename = "sitka_weather_07-2014.csv"
+# filename = "sitka_weather_07-2014.csv"
+filename = "sitka_weather_2014.csv"
+
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -12,6 +14,7 @@ with open(filename) as f:
 
     highs = []
     dates = []
+    lows = []
 
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
@@ -20,16 +23,21 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     print(highs)
 
 # plot data
 
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(dates, highs, c="red")
+plt.plot(dates, highs, c="red", alpha=0.5)
+plt.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha = 0.1)
 
 # format plot
 
-plt.title("Temperaturas altas de julio 2014", fontsize = 24)
+plt.title("Temperaturas de 2014", fontsize = 24)
 plt.xlabel('', fontsize = 16)
 fig.autofmt_xdate()
 plt.ylabel("Temperatura (F)", fontsize = 16)
