b46cfb247184ad797877cab079abd0164b4d41b0
Diego Aarón <diegoaarondv@gmail.com>
2023-05-07 22:31:41 -0500

    book 1 - 112

--- pyt_crash_course/learning_log/learning_logs/views.py
+++ pyt_crash_course/learning_log/learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -11,3 +14,25 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Muestra un unico topic y todos sus items"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # No data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # Post data submitted 
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+        
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
