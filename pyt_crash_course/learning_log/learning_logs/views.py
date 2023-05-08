a9c1ebd2d85a585431b5b1cdabfcf22106835c8c
Diego Aarón <diegoaarondv@gmail.com>
2023-05-08 00:05:35 -0500

    book 1 - 113

--- pyt_crash_course/learning_log/learning_logs/views.py
+++ pyt_crash_course/learning_log/learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Topic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -36,3 +36,22 @@ def new_topic(request):
         
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # Post data submitted; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
