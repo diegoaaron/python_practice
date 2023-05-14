c17b2d523e301e4e6b31fd090001293d66c28faa
Diego Aarón <diegoaarondv@gmail.com>
2023-05-14 18:20:12 -0500

    book 1 - 128

--- pyt_crash_course/learning_log/learning_logs/models.py
+++ pyt_crash_course/learning_log/learning_logs/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     """Lo que esta aprendiendo el usuario"""
     text = models.CharField(max_length=200)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
-
-
 
     def __str__(self):
         """Retorna una cadena que representa al objeto del modelo"""
--- pyt_crash_course/learning_log/learning_logs/views.py
+++ pyt_crash_course/learning_log/learning_logs/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render
 from .models import Topic, Entry
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     """The home page for learning log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Muestra todo los topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner = request.user).order_by('date_added')
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Muestra un unico topic y todos sus items"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
@@ -37,6 +43,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -56,6 +63,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form} 
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edita una entrada existente"""
     entry = Entry.objects.get(id = entry_id)
