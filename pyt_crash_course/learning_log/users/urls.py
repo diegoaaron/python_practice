bcbd5eccc817123e8cab550b999002e3bdbc5daa
Diego Aarón <diegoaarondv@gmail.com>
2023-05-14 17:54:59 -0500

    book 1 - 127

--- pyt_crash_course/learning_log/users/urls.py
+++ pyt_crash_course/learning_log/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.MyLoginView.as_view(template_name='users/login.html'), name = 'login'),
     path('logout/', views.logout_view, name = 'logout'),
+    path('register/', views.register, name = 'register'),
 ]
--- pyt_crash_course/learning_log/users/views.py
+++ pyt_crash_course/learning_log/users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 class MyLoginView(LoginView):
@@ -20,3 +22,21 @@ class MyLoginView(LoginView):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    """Register a new user"""
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data = request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username = new_user.username,
+                                              password = request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+    
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
+
