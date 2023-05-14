745245f5217692c231fb977797ef352559ffdcdb
Diego Aarón <diegoaarondv@gmail.com>
2023-05-14 17:39:24 -0500

    book 1 - 126

--- pyt_crash_course/learning_log/users/views.py
+++ pyt_crash_course/learning_log/users/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 class MyLoginView(LoginView):
@@ -12,3 +15,8 @@ class MyLoginView(LoginView):
     def form_invalid(self, form):
         messages.error(self.request, 'Usuario o pass invalido')
         return self.render_to_response(self.get_context_data(form=form))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
