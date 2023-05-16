6f2f922f8aec54879e7e65b4f1aee85de47d09b1
Diego Aarón <diegoaarondv@gmail.com>
2023-05-15 23:03:39 -0500

    book 2 - 2

--- boring_stuff/practice_class/step1.py
+++ boring_stuff/practice_class/step1.py
@@ -5,3 +5,8 @@ print(perry)
 def ladrar():
     pass
 
+# parametro: elemento definido en una función, necesario para su funcionamiento
+
+# argumento: valor pasado a una función cuando es invocada.
+
+# Tipos de argumentos
