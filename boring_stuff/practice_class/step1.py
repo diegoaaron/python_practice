548f30f3afecf13d67125f6373f903a6ffea3a22
Diego Aarón <diegoaarondv@gmail.com>
2023-05-26 13:15:28 -0500

    book 2 - 16

--- boring_stuff/practice_class/step1.py
+++ boring_stuff/practice_class/step1.py
@@ -5,7 +5,8 @@
 # Las clases deben ser nombradas con el formato CamelCase
 # Las instancias de clase y modulos deben nombrarse en minusculas y separadas por _
 
-
+# variable: forma más básica de almacenar información
+# pass: palabra reservada que funciona como relleno de codigo
 
 perry = "toffy"
 
@@ -18,9 +19,10 @@ def ladrar():
 
 # argumento: valor pasado a una función cuando es invocada.
 
-# Tipos de argumentos
+# Tipos de configuración para los argumentos 
 
-# argumento posicional: es la configuración por defecto y consiste en pasar los argumentos en el orden en que sus parámetros están definidos.
+# argumento posicional: es la configuración por defecto y consiste en pasar los argumentos en el orden en que sus parámetros 
+# están definidos.
 
 def descripcion_mascota(raza, nombre):
     print("La mascota es: " + nombre)
@@ -28,7 +30,8 @@ def descripcion_mascota(raza, nombre):
 
 descripcion_mascota("pastor aleman", "toffy")
 
-# argumento keywoard: Es cuando un argumento es pasado en el formato "parametro=arugmento", haciendo irrelevante el orden en que se definieron los parametros al crear la funcion. 
+# argumento keywoard: Es cuando un argumento es pasado en el formato "parametro=argumento", haciendo irrelevante el orden 
+# en que se definieron los parametros al crear la funcion. 
 
 print("-----------------------1")
 
@@ -38,3 +41,15 @@ def des_mascota(raza, nombre):
 
 des_mascota(nombre="toffina", raza="dogo")
 
+des_mascota("doberman", nombre="toffy")
+
+# argumento por defecto: una función puede tener parametros con valores por defecto, los cuales su utilizadas si al invocar la función no se le pasa un argumento para ese parámetro. Los parametros con valores por defecto deben definirse al final.
+
+print("-----------------------2")
+
+def descrip_mascota(nombre, raza="pequines"):
+    print("mi mascota es " + nombre)
+    print("es de raza: " + raza)
+
+descrip_mascota("toffy")
+descrip_mascota("toffy", "doberman")
