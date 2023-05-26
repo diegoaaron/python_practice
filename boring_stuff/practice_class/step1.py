ec778a0d350bca96ea5b4f8fd40f33678594bdc7
Diego Aarón <diegoaarondv@gmail.com>
2023-05-26 12:59:29 -0500

    book 2 - 15

--- boring_stuff/practice_class/step1.py
+++ boring_stuff/practice_class/step1.py
@@ -1,3 +1,12 @@
+# Escribir la identación utilizando 4 espacios y no un TAB (configurar TAB para que haga 4 espacios)
+# Se debe utilizar 80 caracteres como máximo por linea (algunos utilizano 100 caracteres)
+# Utilizar las lineas en blanco para facilitar la lectura de codigo
+# Utilizar un espacio para realizar compraciones como IF AGE < 4: en vez de iF AGE<4:
+# Las clases deben ser nombradas con el formato CamelCase
+# Las instancias de clase y modulos deben nombrarse en minusculas y separadas por _
+
+
+
 perry = "toffy"
 
 print(perry)
